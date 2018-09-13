bc8b3da9ccaf6c5c4c5a02b18a554d604b580b19
Paul <swiergoszp@gmail.com>
2018-09-13 11:08:43 -0600

    init branch

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,54 @@
-// var db = require("../models");
+module.exports = function(app, passport) {
 
-module.exports = function(app) {
-  // Load index page
+  // Load start page
   app.get("/", function(req, res) {
-    res.render("index");
+      res.render("start");
+  });
+
+  app.get('/login', function(req,res){
+      res.render('login'); 
+  });
+
+  app.post('/login', passport.authenticate('local-signin', {
+      successRedirect: '/select',
+      failureRedirect: '/login'
+  }));
+
+  app.get('/signup', function(req,res){
+      res.render('signup'); 
+  });
+  
+  app.post('/signup', passport.authenticate('local-signup', {
+      successRedirect: '/select',
+      failureRedirect: '/signup'
+  }));
+
+  app.get('/select',isLoggedIn, function(req,res){
+      res.render('select'); 
   });
 
   // Load example page and pass in an example by id
   app.get("/books", function(req, res) {
-    res.render("book");
+      res.render("book");
   });
 
   app.get("/user", function(req, res) {
-    res.render("user");
+      res.render("user");
   });
+  
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+      res.render("404");
   });
+
+  app.get('/logout', function(req,res){
+      res.render('logout'); 
+  });
+
+  function isLoggedIn(req, res, next) {
+      if (req.isAuthenticated())
+          return next();  
+      res.redirect('/login');
+  };
+   
 };
