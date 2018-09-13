041a899b3acea3a1e455f89cdd1f2d3ca0f4eca9
PatMiranda <patrick_miranda@ymail.com>
2018-09-13 09:51:44 -0700

    added the parent site and fixed a pathing issue on index

---
+++ public/js/parent.js
@@ -0,0 +1,18 @@
+$("#buyMoon").on("click", function(){
+    $("#buyMoon").css("display", "none");
+});
+$("#buyMouse").on("click", function(){
+    $("#buyMouse").css("display", "none");
+});
+$("#buyPigeon").on("click", function(){
+    $("#buyPigeon").css("display", "none");
+});
+$("#buyGas").on("click", function(){
+    $("#buyGas").css("display", "none");
+});
+$("#buyPooh").on("click", function(){
+    $("#buyPooh").css("display", "none");
+});
+$("#buyBears").on("click", function(){
+    $("#buyBears").css("display", "none");
+});
